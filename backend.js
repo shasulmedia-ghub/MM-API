00a97a1b58a89b3fff3d8a0fd177e20f992d5a53
shasulmedia-ghub <shasul.media@gmail.com>
2026-07-28 22:39:25 +0800

    added cart and updated backend

--- backend.js
+++ backend.js
@@ -1,5 +1,6 @@
 const express = require("express"); 
 const cors = require("cors"); 
+const cart = require('./cart');
 
 const app = express(); 
 
@@ -12,5 +13,13 @@ app.post("/register", require("./api/register"));
 // Login user API
 app.post("/login", require("./api/login"));
 
+// Cart APIs
+app.get('/api/cart/:userId', cart.getCart);
+app.get('/api/cart/:userId/summary', cart.getCartSummary);
+app.post('/api/cart', cart.addToCart);
+app.put('/api/cart/:cartId', cart.updateCartItem);
+app.delete('/api/cart/:cartId', cart.removeCartItem);
+app.delete('/api/cart/user/:userId', cart.clearCart);
+app.delete('/api/cart/items', cart.removeSelectedItems);
 
 module.exports = app;
