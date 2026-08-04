e013e805b7b08e1c5bf49012643dce9c64db0f49
shasulmedia-ghub <shasul.media@gmail.com>
2026-08-05 00:09:24 +0800

    update bcrypt

--- api/users.js
+++ api/users.js
@@ -1,5 +1,5 @@
 // users.js
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const pool = require('./db');
 const cors = require("./cors");
