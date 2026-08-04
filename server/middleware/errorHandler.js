c8165b02405a1104653c2d948f68aeb2eef921fc
Johnny <chuaboonhock@singnet.com.sg>
2026-08-04 20:22:52 +0800

    create middleware directory and add errorhandler.js

---
+++ server/middleware/errorHandler.js
@@ -0,0 +1,13 @@
+module.exports = (err, req, res, next) => {
+
+    console.error(err);
+
+    return res.status(500).json({
+
+        success: false,
+
+        message: "Internal Server Error",
+
+    });
+
+};
